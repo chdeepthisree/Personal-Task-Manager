16403abd71a41513bfc320b1405028c093ae6c9b
deepthi <deepthisreechekuri@gmail.com>
2026-07-14 15:56:37 +0530

    Initial Django Task Manager project

---
+++ tasks/views.py
@@ -0,0 +1,39 @@
+
+
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+@login_required
+def task_create(request):
+    form = TaskForm(request.POST or None)
+    if form.is_valid():
+        task = form.save(commit=False)
+        task.user = request.user
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+@login_required
+def task_update(request, pk):
+    task = Task.objects.get(pk=pk)
+    form = TaskForm(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('task_list')
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+@login_required
+def task_delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
+
